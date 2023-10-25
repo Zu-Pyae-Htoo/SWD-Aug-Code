ded84ba513e93ef14f4037f6fe0a9bd2c7710741
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-10-26 01:07:09 +0630

    day-41 record finished

---
+++ 4-JavaScript/js/date.js
@@ -0,0 +1,21 @@
+console.dir(Date);
+
+const date = new Date;
+// console.dir(date);
+
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+
+// console.log((date.getFullYear()));
+// console.log((date.getFullYear()));
+// console.log((date.getDate()));
+// console.log(date.getDay());
+
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+//Jan 1 1970 --> milliseconds စရေ
+
+// console.log(date.getTime());
+// console.log(Date.now());
