02dd1d6b14bcb0419de98900f7f6795d8d7a925d
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-10-26 23:38:24 +0630

    date obj finished

--- 4-JavaScript/js/date.js
+++ 4-JavaScript/js/date.js
@@ -1,6 +1,6 @@
-console.dir(Date);
+// console.dir(Date);
 
-const date = new Date;
+// const date = new Date;
 // console.dir(date);
 
 // console.log(date.toDateString());
@@ -19,3 +19,32 @@ const date = new Date;
 
 // console.log(date.getTime());
 // console.log(Date.now());
+
+// =============================================
+// Day-41 Oct26/2023
+
+// const date = new Date("1998-3-3");
+// const date = new Date(2050,2,3);
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getDay());
+
+
+// const date = new Date();
+
+// အခုမှစ နောက် 300hr ဘယ်နေ့လဲ
+//ပြင်ချင်ရင် set နဲ့ပြင် 
+// date.setHours(date.getHours()+300)
+
+//Blood donation for next time is 4 months
+// date.setMonth(date.getMonth()+4)
+// console.log(date);
+
+// 50hrs SWD course 2 hrs/day = 25 day
+// date.setDate(date.getDate()+25);
+// console.log(date);
