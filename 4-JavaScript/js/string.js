f2969304fda76f356cc8c7dcfeeb92eb28c2f1bc
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-10-28 01:47:37 +0630

    padStart and padEnd finished

---
+++ 4-JavaScript/js/string.js
@@ -0,0 +1,43 @@
+const str = new String("     I'm hein htet zan from mms it   ");
+console.log(str);
+// console.log(str[14]);
+// console.log(str[15]);
+// console.log(str[16]);
+// console.log(str.length);
+
+//လိုချင်တဲ့ char ကို index နဲ့ရှာတာ
+//first match index ပဲပြ
+// console.log(str.substr(4,13));
+// console.log(str.substr(23,28));
+// console.log(str.substring(4,17));
+// console.log(str.slice(18,29));
+// console.log(str.slice(4,29));
+
+// index ကို နဲ့ char ရှာ 
+// -1 index ဆို charမရှိ
+// console.log(str.search("h"));
+// console.log(str.search('I'));
+// console.log(str.search("z"));
+// console.log(str.search("B"));
+
+// console.log(str.replace('htet','kyar'));
+//first match
+// console.log(str.replace('m',"000"));
+//replace all
+// console.log(str.replaceAll("m","00"));
+
+// console.log(str.toLocaleUpperCase());
+// console.dir(str.toLocaleLowerCase());
+
+
+// Trim 
+// ==============
+// console.log(str.length);
+// let text = str.trim();
+// console.log(text);
+// console.log(text.length);
+
+// 0000 0000 => 000 0001 - 9999 9999
+// const num = "1";
+// const text = num.padEnd(8,"0");
+// console.log(text);
