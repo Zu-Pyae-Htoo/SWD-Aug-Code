a73510eacdf138ac7351668a5436f8917068ebb2
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-11-02 16:56:20 +0630

    arrow function

---
+++ 4-JavaScript/js/array.js
@@ -0,0 +1,58 @@
+// Day-43 Nov2/2023
+// =====================
+
+// call back function
+// ======================
+
+// function x() {
+//   return "x";
+// }
+// function y(callback) {
+//   return callback();
+// }
+// console.log(y(x));
+
+// function run(){
+//     return 'hello';
+// }
+// function test(callback){
+//    return callback()+' zu pyae htoo'
+// }
+
+// console.log(test(run));
+// ================
+
+const points = [34, 15, 2, 52, 19, 56, 30, 57];
+const fruits = ["apple","orange","mango","lemon","lime"]
+
+// for(let point of points){
+//     console.log(point);
+// }
+
+// console.dir(Array);
+
+// console.log(points);
+
+// function run(value,index,array){
+//     console.log(value,index,array);
+// };
+
+// points.forEach(run);
+
+// function x(fruit,list,store){
+//     console.log(fruit,list,store);
+// }
+// fruits.forEach(x);
+
+// points.forEach(function(value){
+//     console.log(value);
+// })
+
+// fruits.forEach(function(value){
+//     console.log(value);
+// })
+// ======================
+
+//Arrow Function
+// ================
+
