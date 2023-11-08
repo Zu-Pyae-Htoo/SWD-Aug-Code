db79d85a0849ead3a712b783ca28e6ccc04994d6
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-11-08 22:55:13 +0630

    day45 finished

---
+++ 4-JavaScript/js/func.js
@@ -0,0 +1,29 @@
+// console.dir(Function);
+
+// const area =function(w,h)  {
+//     console.log(arguments);
+//     return w * h;
+// }
+
+// console.log(area(15,50,13,40,60));
+// console.dir(area)
+// console.log(area.name);
+// console.log(area.bind(null,15,50).apply());
+// console.log(area.call(null,15,50));
+
+
+//Rest Parameter <= function ရဲ့ parameter မှာပဲသုံးတယ်
+// ================
+// const run = (a,b,...c)=>{
+//     console.log(a,b,c);
+// }
+// run('a','b','c','d','e','f','g')
+
+//Obj Prototype
+// ===============
+
+// const obj = {a : "aaa", b: "bbb",c : "ccc"}
+
+// console.dir(Object);
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
