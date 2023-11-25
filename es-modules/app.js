d60fc9c1b9a7c6e3fe6b7b683234987496160b42
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-11-25 17:21:05 +0630

    day55 modules finished

--- es-modules/app.js
+++ es-modules/app.js
@@ -1,23 +1,39 @@
-// 1
+// No1
 // Modules import
 // =================
 // data.js and run.js ကနေ export လုပ်ထားဖို့လို။ <script> type မှာ module ပေးထားဖို့လို
 
-    // import * as data from "./js/data.js";
-    // import * as run from "./js/run.js";
 
-// 2
+
+import * as data from "./js/data.js";
+import * as run from "./js/run.js";
+
+// No2
 //<---- Destructure Modules------>
 // ==================================
 
-// import {myAge} from "./js/data.js";
-// import {myName,myWork} from "./js/run.js";
+import {myAge} from "./js/data.js";
+import {myName,myWork} from "./js/run.js";
 
-    // console.log(data.myAge);
-    // console.log(run.myName);
-    // console.log(run.myWork);
+// console.log(data.myAge);
+// console.log(run.myName);
+// console.log(run.myWork);
 
-    // console.log(myAge);
-    // console.log(myName);
-    // console.log(myWork);
+// console.log(myAge);
+// console.log(myName);
+// console.log(myWork);
+
+//No3
+// export default Modules
+// ======================
+//Class တခုတည်းရှိတဲ့ချိန် function တခုတည်းရှိတဲ့ချိန် export default  သုံးတယ်။
+
+import area, { circle } from "./js/area.js";
+import Person from "./js/Person.js";
+
+
+// console.log(area(20,20));
+// console.log(circle);
+const p1 = new Person;
+
 
---
+++ es-modules/js/area.js
@@ -0,0 +1,13 @@
+// No-3
+// export default
+// =================
+
+const area = (w,b) => {
+    return w*b
+}
+
+export default area;
+
+export const circle = (r) => {
+    return Math.PI*r**2;
+}
