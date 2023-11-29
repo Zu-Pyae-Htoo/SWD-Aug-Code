65e4b96369d49e39070e7f32dc665745d0542a09
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-11-29 20:07:11 +0630

    increase and decrease btn finished

--- 6-vite/vite-invoice/src/app/record.js
+++ 6-vite/vite-invoice/src/app/record.js
@@ -1,28 +1,38 @@
 import { recordTotal, templateForRecordList } from "../core/selector";
 
-export const recordUI = (id,name,price,quantity) => {
+export const recordUI = (id, name, price, quantity) => {
   const record = templateForRecordList.content.cloneNode(true);
-  const cost = price*quantity;
-  record.querySelector("tr").setAttribute("record-id",id);
+  const cost = price * quantity;
+  record.querySelector("tr").setAttribute("record-id", id);
   record.querySelector(".record-name").innerText = name;
   record.querySelector(".record-price").innerText = price;
   record.querySelector(".record-quantity").innerText = quantity;
   record.querySelector(".record-cost").innerText = cost;
   return record;
-}
+};
 
+export const updateRecord = (id, quantity) => {
+  const row = app.querySelector(`tr[product-id='${id}']`);
+  const rowQuantity = row.querySelector(".record-quantity");
+  const price = row.querySelector(".record-price");
+  const cost = row.querySelector(".record-cost");
+  if(quantity>0 || rowQuantity.innerText> 1){
+    rowQuantity.innerText = parseInt(rowQuantity.innerText)+quantity;
+    console.log(price,rowQuantity);
+   cost.innerText = price.innerText *rowQuantity.innerText;
+  }
+   calRecordTotal();
+};
 
+export const calRecordTotal = () => {
+  // let total = 0;
+  // const costs = app.querySelectorAll(".record-cost");
+  // costs.forEach((el) => (total += parseFloat(el.innerText)));
 
-  export const calRecordTotal = () => {
-    // let total = 0;
-    // const costs = app.querySelectorAll(".record-cost");
-    // costs.forEach((el) => (total += parseFloat(el.innerText)));
-  
-    const total = [...document.querySelectorAll(".record-cost")].reduce(
-      (pv, cv) => pv + parseFloat(cv.innerText),
-      0
-    );
-    recordTotal.innerText = total;
-    return total;
-  };
-  
+  const total = [...document.querySelectorAll(".record-cost")].reduce(
+    (pv, cv) => pv + parseFloat(cv.innerText),
+    0
+  );
+  recordTotal.innerText = total;
+  return total;
+};
--- 6-vite/vite-invoice/src/core/handler.js
+++ 6-vite/vite-invoice/src/core/handler.js
@@ -1,110 +1,86 @@
 import { templateUI } from "../app/products";
-import { calRecordTotal, recordUI } from "../app/record";
-import { app, drawer, newProductForm, productSelect, quantityInput, recordForm, recordGroup } from "./selector";
+import { calRecordTotal, recordUI, updateRecord } from "../app/record";
+import {
+  app,
+  drawer,
+  newProductForm,
+  productSelect,
+  quantityInput,
+  recordForm,
+  recordGroup,
+} from "./selector";
 import { products } from "./variables";
 
 export const productDrawerHandler = () => {
-    drawer.classList.toggle("translate-x-full");
-    drawer.classList.add("duration-200");
-  };
+  drawer.classList.toggle("translate-x-full");
+  drawer.classList.add("duration-200");
+};
 
 export const newProductFormHandler = (event) => {
-    event.preventDefault();
-    const formData = new FormData(newProductForm);
-  
-    // console.log(formData.get("new_product_name"));
-    // console.log(formData.get("new_product_price"));
-  
-    const newProduct = {
-      id: Date.now(),
-      name: formData.get("new_product_name"),
-      price: formData.get("new_product_price"),
-    };
-  
-    productGroup.append(templateUI(newProduct));
-    productSelect.append(new Option(newProduct.name, newProduct.id));
-    products.push(newProduct);
-  
-    newProductForm.reset();
+  event.preventDefault();
+  const formData = new FormData(newProductForm);
+
+  // console.log(formData.get("new_product_name"));
+  // console.log(formData.get("new_product_price"));
+
+  const newProduct = {
+    id: Date.now(),
+    name: formData.get("new_product_name"),
+    price: formData.get("new_product_price"),
   };
 
-  export const recordFormHandler = (event) => {
-    event.preventDefault();
-    // idကနေ => product(objအခန်း)ရှာ => price,name
-    let currentProduct = products.find(
-      (product) => product.id == productSelect.value
-    );
-  
-    // console.log(productSelect.value);
-    // console.log(currentProduct.name);
-    // console.log(quantityInput.valueAsNumber);
-    const isExistProduct = app.querySelector(
-      `[product-id='${currentProduct.id}']`
-    );
-  
-    if (isExistProduct) {
-      const currentRow = event.target.closest("tr");
-      const currentRowQ = isExistProduct.querySelector(".record-q");
-      const currentRowCost = isExistProduct.querySelector(".record-cost");
-      const currentRowPrice = isExistProduct.querySelector(".record-price");
-  
-      currentRowQ.innerText =
-        parseInt(currentRowQ.innerText) + quantityInput.valueAsNumber;
-      currentRowCost.innerText =
-        currentRowPrice.innerText * currentRowQ.innerText;
-      calRecordTotal();
-  
-      calRecordTotal();
-    } else {
-      recordGroup.append(
-        recordUI(
-          currentProduct.id,
-          currentProduct.name,
-          currentProduct.price,
-          quantityInput.valueAsNumber
-        )
-      );
-      calRecordTotal();
-    }
-  
-    recordForm.reset();
-  };
-  
-  
-  export const recordGroupHandler = (event) => {
-    // console.log(event.target);
-    // const x = event.target.classListcontains("record-del");
-    //   console.log(x);
-    if (event.target.classList.contains("record-del")) {
-      console.log(event.target.closest("tr").remove());
-      calRecordTotal();
-    } else if (event.target.classList.contains("add-q")) {
-      // console.log(event.target);
-      const currentRow = event.target.closest("tr");
-      const currentRowQ = currentRow.querySelector(".record-q");
-      const currentRowCost = currentRow.querySelector(".record-cost");
-      const currentRowPrice = currentRow.querySelector(".record-price");
-  
-      currentRowQ.innerText = parseInt(currentRowQ.innerText) + 1;
-      currentRowCost.innerText =
-        currentRowPrice.innerText * currentRowQ.innerText;
-      calRecordTotal();
-    } else if (event.target.classList.contains("sub-q")) {
-      // console.log(event.target);
-      const currentRow = event.target.closest("tr");
-      const currentRowQ = currentRow.querySelector(".record-q");
-      const currentRowCost = currentRow.querySelector(".record-cost");
-      const currentRowPrice = currentRow.querySelector(".record-price");
-  
-      if (currentRowQ.innerText > 0) {
-        currentRowQ.innerText = parseInt(currentRowQ.innerText) - 1;
-        currentRowCost.innerText =
-          currentRowPrice.innerText * currentRowQ.innerText;
-        calRecordTotal();
-      }
-    }
-  };
+  productGroup.append(templateUI(newProduct));
+  productSelect.append(new Option(newProduct.name, newProduct.id));
+  products.push(newProduct);
 
-  export const printBtnHandler = () => {
-    print();
-  };
+  newProductForm.reset();
+};
+
+export const recordFormHandler = (event) => {
+  event.preventDefault();
+  // idကနေ => product(objအခန်း)ရှာ => price,name
+  let currentProduct = products.find(
+    (product) => product.id == productSelect.value
+  );
+
+  // console.log(productSelect.value);
+  // console.log(currentProduct.name);
+  // console.log(quantityInput.valueAsNumber);
+  const isExistProduct = app.querySelector(
+    `[product-id='${currentProduct.id}']`
+  );
+
+  if (isExistProduct) {
+    updateRecord(currentProduct.id, quantityInput.valueAsNumber);
+  } else {
+    recordGroup.append(
+      recordUI(
+        currentProduct.id,
+        currentProduct.name,
+        currentProduct.price,
+        quantityInput.valueAsNumber
+      )
+    );
+  }
+
+  calRecordTotal();
+  recordForm.reset();
+};
+
+//delete,increase,decrease button
+export const recordGroupHandler = (event) => {
+  if (event.target.classList.contains("record-del")) {
+    event.target.closest("tr").remove();
+    calRecordTotal();
+  } else if (event.target.classList.contains("add-q")) {
+    const id = event.target.closest("tr").getAttribute("product-id");
+    updateRecord(id, 1);
+  } else if (event.target.classList.contains("sub-q")) {
+    const id = event.target.closest("tr").getAttribute("product-id");
+    updateRecord(id, -1);
+  }
+};
+
+export const printBtnHandler = () => {
+  print();
+};
