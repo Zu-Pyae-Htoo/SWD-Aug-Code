c187cabf56e258b0dfe05d6ec33b7579654188ba
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-12-03 16:41:07 +0630

    star

--- 6-vite/vite-shopping/src/app/product.js
+++ 6-vite/vite-shopping/src/app/product.js
@@ -16,8 +16,7 @@ export const productUi = ({
   product.querySelector(".product-price").innerText = price;
   product.querySelector(".product-rate").innerText = rate;
   product.querySelector(".product-count").innerText = count;
-  product.querySelector(".product-stars").innerHtml = starRating(rate);
-  console.log(starRating(rate));
+  product.querySelector(".product-stars").innerHTML = starRating(rate);
   return product;
 };
 
@@ -26,7 +25,7 @@ export const productRender = (lists) => {
 };
 
 export const starRating = (rate) => {
-    const fillStar = `     <svg
+    const fillStar = `<svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     fill="currentColor"
@@ -38,7 +37,7 @@ export const starRating = (rate) => {
       clip-rule="evenodd"
     />
   </svg>`;
-    const outlineStar = `    <svg
+    const outlineStar = `<svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
@@ -52,10 +51,15 @@ export const starRating = (rate) => {
       d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
     />
   </svg>`
-
+  
   let stars = "";
   for(let i=1;i<=5;i++){
-    stars += fillStar;
+    if(i<=rate.toFixed(0)){
+        stars += fillStar;
+    }else{
+      stars += outlineStar;
+    }
+    
   }
 
   return stars;
