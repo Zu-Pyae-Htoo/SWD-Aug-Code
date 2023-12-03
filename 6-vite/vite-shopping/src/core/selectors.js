6900a913cbe8b938adad21e03fed62e4aa45af36
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-12-03 23:19:54 +0630

    cat-btn

--- 6-vite/vite-shopping/src/core/selectors.js
+++ 6-vite/vite-shopping/src/core/selectors.js
@@ -3,3 +3,5 @@ export const categoryGroup = app.querySelector("#categoryGroup");
 export const categoryTemplate = app.querySelector("#categoryTemplate");
 export const productGroup = app.querySelector("#productGroup");
 export const productTemplate = app.querySelector("#productTemplate");
+export const fillStarTemplate = app.querySelector("#fillStarTemplate");
+export const outlineStarTemplate = app.querySelector("#outlineStarTemplate");
