009ee941e5e95ec2fc965f6c10397a72ed3013e7
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-12-05 17:55:21 +0630

    modal

---
+++ 6-vite/vite-flowbite/main.js
@@ -0,0 +1,65 @@
+// import { Carousel } from "flowbite";
+import "./style.css";
+import "./node_modules/flowbite/dist/flowbite"
+// import './node_modules/flowbite/dist/flowbite'
+
+// const slideUi = document.querySelector("#my-slide");
+// const uSlide = document.querySelector("#u-slide");
+// const nextSlide = document.querySelector("#next-slide");
+// const prevSlide = document.querySelector("#prev-slide");
+
+// const items = [
+//   {
+//     position: 0,
+//     el: document.getElementById("my-slide-1"),
+//   },
+//   {
+//     position: 1,
+//     el: document.getElementById("my-slide-2"),
+//   },
+//   {
+//     position: 2,
+//     el: document.getElementById("my-slide-3"),
+//   },
+//   {
+//     position: 3,
+//     el: document.getElementById("my-slide-4"),
+//   },
+// ];
+
+// const uSlideitems = [
+//   {
+//     position: 0,
+//     el: document.getElementById("u-slide-1"),
+//   },
+//   {
+//     position: 1,
+//     el: document.getElementById("u-slide-2"),
+//   },
+//   {
+//     position: 2,
+//     el: document.getElementById("u-slide-3"),
+//   },
+//   {
+//     position: 3,
+//     el: document.getElementById("u-slide-4"),
+//   },
+// ];
+
+// options = {
+//   defaultPosition: 1,
+//   interval: 1000,
+// };
+
+// const slider = new Carousel(slideUi, items,options);
+// const uSlider = new Carousel(uSlide, uSlideitems,options);
+
+// nextSlide.addEventListener("click", () => {
+//   slider.next();
+//   uSlider.prev();
+// });
+
+// prevSlide.addEventListener("click", () => {
+//   slider.prev();
+//   uSlider.next();
+// });
