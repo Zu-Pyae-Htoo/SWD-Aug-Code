1517089c8b165b7b72c0d9a9993b08c083129a8a
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-12-08 17:07:13 +0630

    day-63 finished

--- 6-vite/vite-libraries/main.js
+++ 6-vite/vite-libraries/main.js
@@ -2,7 +2,9 @@ import Typed from "typed.js";
 import "./style.css";
 import AOS from "aos";
 import WOW from "wow.js";
-import Glide from '@glidejs/glide'
+import Glide from "@glidejs/glide";
+import { Chart } from "chart.js/auto";
+import "./node_modules/waypoints/lib/noframework.waypoints";
 
 const typeUi = document.querySelector("#typeUi");
 const options = {
@@ -29,13 +31,57 @@ const wow = new WOW({
 
 wow.init();
 
-
-
-new Glide('.glide', {
-  type: 'carousel',
-  focusAt : 'center' ,
+new Glide(".glide", {
+  type: "carousel",
+  focusAt: "center",
   startAt: 2,
-  perView: 2,
-  gap : 30,
-  animationTimingFunc : 'ease-in-out',
-}).mount()
+  perView: 3,
+
+  animationTimingFunc: "ease-in-out",
+}).mount();
+
+const myChart = document.querySelector("#myChart");
+const chart = new Chart(myChart, {
+  type: "line",
+  data: {
+    labels: ["mudon", "mlm", "ygn", "mdy", "tgy", "kalaw"],
+    datasets: [
+      { label: "2021 citizens", data: [12, 19, 3, 5, 2, 3], borderWidth: 1 },
+      { label: "2022 citizens", data: [12, 10, 7, 5, 2, 1], borderWidth: 1 },
+      { label: "2023 citizens", data: [18, 15, 3, 9, 13, 6], borderWidth: 1 },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+const myTest = document.querySelector("#myTest");
+const adObj = document.querySelector("#adObj");
+const nextSection = document.querySelector("#nextSection");
+
+const wayPoint = new Waypoint({
+  element: myTest,
+  handler(direction) {
+    if (direction == "down") {
+      adObj.classList.remove("translate-x-full");
+    } else {
+      adObj.classList.add("translate-x-full");
+    }
+  },
+  offset: "50%",
+});
+
+const nextSectionWayPoint = new Waypoint({
+  element: nextSection,
+  offset: 20,
+  handler(direction) {
+    if (direction == "down") {
+      adObj.classList.add("translate-x-full");
+    }
+  },
+});
