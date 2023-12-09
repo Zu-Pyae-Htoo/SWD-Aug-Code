0c77617de50293cfd6bcf190038c292a649328bd
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-12-10 00:01:39 +0630

    react-app

--- 7-react/First-Test/main.js
+++ 7-react/First-Test/main.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import './style.css';
-import ReactDOM  from 'react-dom/client';
-
+import React from "react";
+import "./style.css";
+import ReactDOM from "react-dom/client";
 
 const rootElement = document.querySelector("#root");
 
 const root = ReactDOM.createRoot(rootElement);
 
-const heading = React.createElement("h1",null,"Hello") //ReactNode
+// const heading = React.createElement("h1",null,"Hello") //ReactNode
 
-root.render(heading)
+const App = () => {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("h1", null, "Courses"),
+    React.createElement(
+      "ul",
+      null,
+      React.createElement("li", null, "SWD"),
+      React.createElement("li", null, "WAD"),
+      React.createElement("li", null, "BUD"),
+      React.createElement("li", null, "PUD"),
+    )
+  );
+};
 
+
+
+
+
+root.render(App());
