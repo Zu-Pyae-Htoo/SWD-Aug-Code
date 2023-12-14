3584f632ec9e62f659ee4308c0fd337e04256964
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-12-15 00:43:08 +0630

    product-drawer UI fixing

--- 7-react/react-invoice/src/App.jsx
+++ 7-react/react-invoice/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Header from "./components/Header";
+import CheckoutForm from "./components/CheckoutForm";
+import RecordTable from "./components/RecordTable";
+import Footer from "./components/Footer";
 
-function App() {
-  const [count, setCount] = useState(0)
+import ProductDrawer from "./components/ProductDrawer";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="max-w-[700px] mx-auto min-h-screen flex flex-col">
+      <Header />
+      <CheckoutForm />
+      <RecordTable />
+      <Footer />
+      <ProductDrawer />
+    </div>
+  );
+};
 
-export default App
+export default App;
