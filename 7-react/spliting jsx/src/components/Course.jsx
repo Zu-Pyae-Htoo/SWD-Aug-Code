85523bd1ff3bba46bceecb050efbd38b1372a947
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2023-12-14 15:29:15 +0630

    react day 2

--- 7-react/spliting jsx/src/components/Course.jsx
+++ 7-react/spliting jsx/src/components/Course.jsx
@@ -2,11 +2,26 @@ import React from "react";
 
 const Course = ({ id, title, count }) => {
   const bgCLassName = count === 0 ? "bg-red-200 border-red-300" : "bg-gray-200";
+
+  //conditional(recommended)
+  // =============
   return (
-    <div className={`mb-3 p-2 border-s-4 border-neutral-400 ${bgCLassName}`}>
-      {title} {count}
-    </div>
+    count > 0 && (
+      <div className={`mb-3 p-2 border-s-4 border-neutral-400 ${bgCLassName}`}>
+        {title} {count}
+      </div>
+    )
   );
+
+  //if else
+  // ==========
+  // if (count > 0) {
+  //   return (
+  //     <div className={`mb-3 p-2 border-s-4 border-neutral-400 ${bgCLassName}`}>
+  //       {title} {count}
+  //     </div>
+  //   );
+  // }
 };
 
 export default Course;
