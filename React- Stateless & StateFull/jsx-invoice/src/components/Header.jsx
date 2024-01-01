33fb100a14b39c9f49a1a28d4a89c2428655da6c
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2024-01-01 17:42:18 +0630

    props before contex

--- React- Stateless & StateFull/jsx-invoice/src/components/Header.jsx
+++ React- Stateless & StateFull/jsx-invoice/src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 
 const Header = () => {
   return (
     <header className="mb-10 mt-10 font-heading">
-      <h1 className="text-3xl font-bold text-gray-700">MMS Solutions</h1>
+      <h1 className="text-3xl font-bold text-gray-700">MMS Solutions </h1>
       <h4 className="text-xl text-gray-500">Invoice App</h4>
     </header>
   );
