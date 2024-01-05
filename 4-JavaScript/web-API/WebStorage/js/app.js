0504d2a00b4d600bf5ec6417faa5a143792084c9
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2024-01-05 11:53:18 +0630

    localStorage

--- 4-JavaScript/web-API/WebStorage/js/app.js
+++ 4-JavaScript/web-API/WebStorage/js/app.js
@@ -2,10 +2,29 @@ const output = document.querySelector("#output");
 const addBtn = document.querySelector("#addBtn");
 const inputText = document.querySelector("#inputText");
 
+(() => {
+  const todo = localStorage.getItem("todo");
+  if (todo) {
+    output.innerHTML = todo;
+  }
+})();
+
 addBtn.addEventListener("click", () => {
   const li = document.createElement("li");
-  li.innerText = inputText.value;
+  const btn = document.createElement("button");
+  btn.innerText = "del";
+  btn.classList.add("del");
+  li.append(btn);
+  const text = document.createTextNode(inputText.value);
+  li.append(text);
   output.append(li);
-  localStorage.setItem(Date.now(),inputText.value)
+  localStorage.setItem("todo", output.innerHTML);
   inputText.value = null;
+});
+
+output.addEventListener("click", (event) => {
+  if (event.target.classList.contains("del")) {
+    event.target.closest("li").remove();
+    localStorage.setItem("todo", output.innerHTML);
+  }
 });
