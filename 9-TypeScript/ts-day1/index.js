5387cb56416acc8dd31ce74a03d4cbe0a5146cd6
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2024-01-14 15:35:27 +0630

    typeScript obj

--- 9-TypeScript/ts-day1/index.js
+++ 9-TypeScript/ts-day1/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+// const num: number = 10;
+// console.log(num);
+// const num2: number = 20;
+// console.log(num+num2);
+// class Person {
+//     name : "zu pyae htoo";
+// }
+var male = true;
+var person = {
+    name: "zu pyae htoo",
+    age: 26,
+    gender: "male",
+};
+// console.log(person.name);
--- 9-TypeScript/ts-day1/index.ts
+++ 9-TypeScript/ts-day1/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World");
+// const num: number = 10;
+// console.log(num);
+
+// const num2: number = 20;
+
+// console.log(num+num2);
+
+// class Person {
+//     name : "zu pyae htoo";
+// }
+
+const male: boolean = true;
+
+const person: {
+  name: string;
+  age: number;
+  gender: string;
+  hello?:string
+} = {
+  name: "zu pyae htoo",
+  age: 26,
+  gender: "male",
+};
+
+// console.log(person.name);
